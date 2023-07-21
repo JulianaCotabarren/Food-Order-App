b7dca15a357f87cf8126e83833caed2dce7acd64
Juliana Cotabarren <julianacotabarren@gmail.com>
2023-07-21 11:51:10 -0300

    feat: add HeaderCartButton

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import meals from '../../assets/meals.jpg';
 import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>Food Order App</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={meals} alt="meals" />
